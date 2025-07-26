0005b803584d33eceb55bb9c93f199f0ea2f071b
Kevin Heredia <kevin@ravecodesolutions.com>
2025-07-26 16:11:50 -0500

    Add scripts

---
+++ ci_scripts/ci_pre_xcodebuild.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+#  ci_pre_xcodebuild.sh
+#  FieldFinder-App
+#
+#  Created by Kevin Heredia on 26/7/25.
+#  
+echo 'Change Beta App Icon'
+
+APP_ICON_PATH="$CI_PRIMARY_REPOSITORY_PATH/FieldFinder-App/Assets.xcassets/AppIcon.appiconset"
+
+echo $APP_ICON_PATH
+
+rm -rf $APP_ICON_PATH
+
+mv "$CI_PRIMARY_REPOSITORY_PATH/ci_scripts/AppIcon-Beta.appiconset" $APP_ICON_PATH
