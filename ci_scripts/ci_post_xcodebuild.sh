c6870b8f41788e22f15f5cd07a7edb9d89b7695f
Kevin Heredia <kevin@ravecodesolutions.com>
2025-07-28 11:09:14 -0500

    refactor code

--- ci_scripts/ci_post_xcodebuild.sh
+++ ci_scripts/ci_post_xcodebuild.sh
@@ -1,6 +1,5 @@
-
 #! /bin/zsh
 
 TESTFLIGHT_DIR_PATH=../TestFlight
 
-git fetch --deepen 3 && git log -3 --pretty=format: "%s" >! $TESTFLIGHT_DIR_PATH/WhatToTest.en-US.txt
+git fetch --deepen 3 && git log -3 --pretty=format:"%s" >! $TESTFLIGHT_DIR_PATH/WhatToTest.en-US.txt
