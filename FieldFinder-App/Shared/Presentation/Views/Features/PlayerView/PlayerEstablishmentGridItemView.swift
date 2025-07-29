0e7edc6c66222d83063fdd85f28ea7568c000896
Kevin Heredia <kevin@ravecodesolutions.com>
2025-07-29 14:33:52 -0500

    bug in favorites button fixed

--- FieldFinder-App/Shared/Presentation/Views/Features/PlayerView/PlayerEstablishmentGridItemView.swift
+++ FieldFinder-App/Shared/Presentation/Views/Features/PlayerView/PlayerEstablishmentGridItemView.swift
@@ -10,20 +10,8 @@ import SwiftUI
 struct PlayerEstablishmentGridItemView: View {
     
     let establishment: EstablishmentResponse
-    
-    @State private var animateFavorite = false
-    
-    @State var viewModel: PlayerGetNearbyEstablishmentsViewModel
-    @State private var isFavorite: Bool
+    @Binding var isFavorite: Bool
     @State var viewModelUser = ProfileUserViewModel()
-
-    init(establishment: EstablishmentResponse, viewModel: PlayerGetNearbyEstablishmentsViewModel = PlayerGetNearbyEstablishmentsViewModel()) {
-
-        self.establishment = establishment
-        self.viewModel = viewModel
-        _isFavorite = State(initialValue: viewModel.isFavorite(establishmentId: establishment.id))
-        
-    }
     
     var body: some View {
         VStack(alignment: .leading, spacing: 10) {
@@ -36,17 +24,7 @@ struct PlayerEstablishmentGridItemView: View {
                 
                 if case .success(let user) = viewModelUser.status, user.rol == "jugador" {
                     
-                    FavoriteButton(isFavorite: $isFavorite ) { newValue in
-                        Task {
-                            isFavorite = newValue
-                            try await viewModel.toggleFavorite(establishmentId: establishment.id, isFavorite: newValue)
-                            try await viewModel.getFavoritesUser()
-                            await MainActor.run {
-                                isFavorite = viewModel.isFavorite(establishmentId: establishment.id)
-                            }
-                        }
-                        
-                    }
+                    FavoriteButton(isFavorite: $isFavorite)
                     .padding(12)
                 }
             }
@@ -78,12 +56,8 @@ struct PlayerEstablishmentGridItemView: View {
             
         }
         .frame(maxWidth: .infinity)
-        .onChange(of: viewModel.favoritesData) { _, _ in
-            isFavorite = viewModel.isFavorite(establishmentId: establishment.id)
-        }
         .onAppear {
             Task {
-                isFavorite = viewModel.isFavorite(establishmentId: establishment.id)
                 try await viewModelUser.getMe()
             }
         }
@@ -97,7 +71,7 @@ struct PlayerEstablishmentGridItemView: View {
 }
 
 #Preview {
-    PlayerEstablishmentGridItemView(establishment: .sample, viewModel: PlayerGetNearbyEstablishmentsViewModel())
+    PlayerEstablishmentGridItemView(establishment: .sample, isFavorite: .constant(true))
 }
 
 
