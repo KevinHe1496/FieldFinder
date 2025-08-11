c431bbca820486a4f23e3ae55dc3181ad440da26
Kevin Heredia <kevin@ravecodesolutions.com>
2025-08-11 12:50:08 -0500

    refactor code

--- FieldFinder-App/Shared/Domain/Tools/ConstantsApp.swift
+++ FieldFinder-App/Shared/Domain/Tools/ConstantsApp.swift
@@ -2,7 +2,7 @@ import Foundation
 
 public struct ConstantsApp {
     // Backend URL
-    public static let CONS_API_URL = "https://fieldfinder-db.fly.dev/api"
+    public static let CONS_API_URL = "http://localhost:8080/api"
     // ID Keychain Token JWT
     public static let CONS_TOKEN_ID_KEYCHAIN = "com.kevinhe.FieldFinder-App"
 }
